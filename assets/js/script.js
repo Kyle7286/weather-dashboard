4b0e43bfd0d3af4ee02a79ab27bfa0ba944ff19b
Brandon Haley <kyle7286@gmail.com>
2021-01-26 16:44:13 -0500

    Figured out Today Card HTML structure; figured out how to display weather icon; have no figured out current date though

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,24 +3,21 @@
 
 
 // Global Declarations
-var searchBox = $("#search-value");
-
-
+var apiKey = "&appid=653447e5538dcc45b8534eb1e5c601c3";
 
 // On Search Click...
 $("#search-button").click(function (e) {
     e.preventDefault();
 
     // Call the current weather function to call the API and build the HTML
-    getCurrentWeather(searchBox.val());
-    
+    getCurrentWeather($("#search-value").val());
+
     // Clear contents of searchbox
-    searchBox.val("");
+    $("#search-value").val("");
 
 });
 
 function getCurrentWeather(city) {
-    var apiKey = "&appid=653447e5538dcc45b8534eb1e5c601c3";
     var cityName = city
     var units = "&units=imperial"
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + units + apiKey
@@ -29,23 +26,69 @@ function getCurrentWeather(city) {
         url: queryURL,
         method: "GET"
     }).then(function (response) {
+        // Log the object for navigating
         console.log(response);
-        var date = "?" // Need luxon
-        var name = response.main.name;
+
+        // Grab the data we need to display
+        var currentDate = response.dt; // Need luxon
+        var name = response.name;
         var currentTemp = response.main.temp;
         var currentHumidity = response.main.humidity;
         var currentWindSpeed = response.wind.speed;
-        var currentUVIndex = "?";
         var currentIcon = response.weather[0].icon;
-        
-        
-        console.log(currentIcon);
+
+        // Create Card
+        var divCard = $("<div>").attr({ class: "card", id: "today-card" });
+        var divCardBody = $("<div>").attr({ class: "card-body", id: "today-card-body" }).appendTo(divCard);
+        var iconURL = "http://openweathermap.org/img/w/" + currentIcon + ".png"
+        console.log(iconURL);
+
+        // Create card elements
+        $("<h3>").text(name + " " + currentDate).appendTo(divCardBody).append($("<img>").attr({ id: "wicon", alt: "Weather Icon" }).attr("src", iconURL));
+        $("<p>").text("Temperature: " + currentTemp + " °F").appendTo(divCardBody);
+        $("<p>").text("Humidity: " + currentHumidity + "%").appendTo(divCardBody);
+        $("<p>").text("Wind Speed: " + currentWindSpeed + "MPH").appendTo(divCardBody);
+
+
+
+
+        // Append final card to page
+        $("#today").append(divCard);
+
+
+
+
+
+
+
+        // Grab Lon & lat values for index call
+        var lon = response.coord.lon;
+        var lat = response.coord.lat;
+
+        // Get UvIndex function
+        getUVIndex(lon, lat);
+
+        // Get 5-day Forecast Function
+
 
     });
 
 
 }
 
-function displayCurrentWeather() {
+function getUVIndex(lon, lat) {
+    // Example URL: http://api.openweathermap.org/data/2.5/uvi?lat={lat}&lon={lon}&appid={API key}
+    var queryURL = "https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + apiKey;
+    var index = "";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        // Log the object for navigating
+        console.log(response);
 
+        // Grab the data we need to display
+        index = response.value;
+        console.log(index);
+    });
 }
